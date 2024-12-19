f3ed734f8951889472563782bced3659132640b8
Dale Bewley <dbewley@redhat.com>
2024-12-18 21:26:54 -0800

    add linux br demo

--- demos/vgt/demo-script-linux-br.sh
+++ demos/vgt/demo-script-linux-br.sh
@@ -5,7 +5,7 @@ source ~/src/demos/demo-magic/demo-magic.sh
 TYPE_SPEED=100
 PROMPT_TIMEOUT=2
 #DEMO_PROMPT="${CYAN}\W${GREEN}➜ ${COLOR_RESET}"
-DEMO_PROMPT="${CYAN}\W-ovs ${GREEN}$ ${COLOR_RESET}"
+DEMO_PROMPT="${CYAN}\W-linbr ${GREEN}$ ${COLOR_RESET}"
 DEMO_COMMENT_COLOR=$GREEN
 GIT_ROOT=$(git rev-parse --show-toplevel)
 DEMO_ROOT=$GIT_ROOT/demos/vgt
@@ -15,25 +15,19 @@ DEMO_ROOT=$GIT_ROOT/demos/vgt
 N=hub-v4tbg-cnv-99zmp
 clear
 
-p "# here is the NNCP to create OVS bridge called 'br-trunk'"
+p "# here is the NNCP to create linux bridge called 'br-trunk'"
 p "# the NIC receives an 802.1q trunk from its switch"
-pei 'bat $DEMO_ROOT/components/br-trunk/ovs-bridge/nncp.yaml'
+pei 'bat $DEMO_ROOT/components/br-trunk/linux-bridge/nncp.yaml'
 
 p
 
-p "# here is the NAD to create a localnet called 'trunk'"
-pei 'bat $DEMO_ROOT/components/trunk/ovs-bridge/nad.yaml'
-
-p
-
-p "# here is the NNCP to create that OVN bridge-mapping"
-p "# which will map the localnet to the ovs bridge"
-pei 'bat $DEMO_ROOT/components/trunk/ovs-bridge/nncp.yaml'
+p "# here is the NAD to create a cnv-bridge called 'trunk'"
+pei 'bat $DEMO_ROOT/components/trunk/linux-bridge/nad.yaml'
 
 p
 
 p "# here is a kustomization overlay to create the bridge and mapping"
-pei "bat $DEMO_ROOT/overlays/ovs-bridge/kustomization.yaml"
+pei "bat $DEMO_ROOT/overlays/linux-bridge/kustomization.yaml"
 
 p
 
@@ -43,20 +37,15 @@ pei "bat -P $DEMO_ROOT/base/kustomization.yaml"
 p
 
 p "# apply the ovs-bridge overlay to create everything including a VM"
-pei "oc apply -k $DEMO_ROOT/overlays/ovs-bridge"
+pei "oc apply -k $DEMO_ROOT/overlays/linux-bridge"
 
 p
 
 p "# confirm results of NNCP and NNCE"
-pei "oc get nncp ovs-bridge-mapping-trunk"
-pei "oc get nnce -l nmstate.io/policy=ovs-bridge-mapping-trunk"
+pei "oc get nncp br-trunk"
+pei "oc get nnce -l nmstate.io/policy=br-trunk"
 
 p "# now that the mapping is defined we can test using the 'trunk' NAD"
 
-DEMO_COMMENT_COLOR=$RED
-p "# FAIL this is not yet possible with ovn-kubernetes localnet"
-p "# https://issues.redhat.com/browse/RFE-6831"
-
-DEMO_COMMENT_COLOR=$GREEN
 
 exit
